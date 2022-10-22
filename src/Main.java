a63edc96270413fec67dea9198fdd32c6a6121e9
netvit <teschkevincent@gmail.com>
2022-10-22 15:53:55 +0200

    second version

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+
+    public static void test(){
+        System.out.println("hello world");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("goodbye world");
+        test();
+        test2("crazy");
+        test2("hello");
+        test2("hello", "crazy");
+        
+        System.out.println("hello");
+
+        HelloFrame frame = new HelloFrame();
+    }
+
+    public static void test2(String sentence_start){
+        System.out.println(sentence_start + ".");
+    }
+
+    public static void test2(String sentence_start, String sentence_end){
+        System.out.println(sentence_start + sentence_end);
+
+    }
+
+}
